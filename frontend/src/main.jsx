dc3f22bd94e4365ec8d1f49704b940bbd540db14
ZAKARIA <zakaria.karaoui@edu.isga.ma>
2026-03-03 00:47:29 +0000

    2nd Deployment Fix

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import {BrowserRouter} from 'react-router-dom'
 import './css/index.css'
 import App from './App.jsx'
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/react"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
